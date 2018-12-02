714d2ce8c09eab1978ff9450102fabd0f06d5fff
Stjepan Petrusa <stjepan.petrusa@gmail.com>
2018-12-02 18:58:31 +0100

    Add npm and webpack info

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'index.js',
+  },
+
+  mode: process.env.NODE_ENV || 'development',
+
+  module: {
+    rules: [{
+      test: /\.scss$/,
+      use: [
+        process.env.NODE_ENV !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader,
+        'css-loader',
+        'sass-loader'
+      ]
+    }, {
+      test: /\.hbs$/,
+      loader: 'handlebars-loader'
+    }, {
+      test: /\.(woff(2)?|ttf|eot|svg)$/,
+      loader: 'file-loader'
+    }]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.hbs'
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ]
+};
