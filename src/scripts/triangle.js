d3d9e0eb72ee779536c34c29b3e5d0de1f75e4a0
Stjepan Petrusa <stjepan.petrusa@gmail.com>
2020-02-21 13:14:39 +0100

    Add terrains to triangles

--- src/scripts/triangle.js
+++ src/scripts/triangle.js
@@ -15,23 +15,42 @@ export const ORIENTATIONS = {
   DOWN: -1
 };
 
+export const TERRAINS = {
+  WATER: 1,
+  GRASS: 2,
+  SAND: 3,
+  FOREST: 4,
+  MOUNTAIN: 5
+}
+
+export const TERRAIN_TO_COLOR = {
+  [TERRAINS.WATER]: (opacity) => `rgba(0, 0, 255, ${opacity})`,
+  [TERRAINS.GRASS]: (opacity) => `rgba(0, 255, 0, ${opacity})`,
+  [TERRAINS.SAND]: (opacity) => `rgba(244, 164, 96, ${opacity})`,
+  [TERRAINS.FOREST]: (opacity) => `rgba(0, 100, 0, ${opacity})`,
+  [TERRAINS.MOUNTAIN]: (opacity) => `rgba(255, 255, 255, ${opacity})`
+}
+
 export default class Triangle {
-  constructor({orientation, row, column}, neighborhood = []) {
+  constructor({orientation, row, column, terrainGradient}, neighbourhood = []) {
     this.orientation = orientation;
     this.row = row;
     this.column = column;
-    this.neighborhood = {};
+    this.neighbourhood = {};
+    this.terrainGradient = terrainGradient;
   }
 
   addNeighbour(position, neighbourTriangle) {
-    this.neighborhood[position] = neighbourTriangle;
+    this.neighbourhood[position] = neighbourTriangle;
   }
 
-  draw(context) {
+  draw(context, percentage) {
     const x = this.column * TRIANGLE_SIZE / 2;
     const y = (this.row + (this.orientation === ORIENTATIONS.UP ? 1 : 0)) * TRIANGLE_HEIGHT;
     const dx = 3 / Math.sqrt(3) * PADDING;
     const dy = 2 * PADDING;
+
+    context.fillStyle = TERRAIN_TO_COLOR[this.terrain](percentage);
 
     context.beginPath();
 
@@ -61,5 +80,20 @@ export default class Triangle {
 
     context.closePath();
     context.fill();
+  }
+
+  get terrain() {
+    const terrainGradient = this.terrainGradient;
+    if (terrainGradient < 0.2) {
+      return TERRAINS.WATER;
+    } else if (terrainGradient < 0.3) {
+      return TERRAINS.SAND;
+    } else if (terrainGradient < 0.6) {
+      return TERRAINS.GRASS;
+    } else if (terrainGradient < 0.8) {
+      return TERRAINS.FOREST;
+    } else {
+      return TERRAINS.MOUNTAIN;
+    }
   }
 }
