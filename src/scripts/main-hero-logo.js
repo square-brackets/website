ca44ca494bf1abe5daa1c4941d765aa3fe72a51d
Stjepan Petrusa <stjepan.petrusa@gmail.com>
2020-03-09 13:44:51 +0100

    Start game after DOM ready

--- src/scripts/main-hero-logo.js
+++ src/scripts/main-hero-logo.js
@@ -1,30 +1,33 @@
 import Game from './game';
 
-const gameTrigger = document.querySelector('.js-game-trigger');
-const gameWrapper = document.querySelector('.js-game-wrapper');
-const canvas = gameWrapper.querySelector('#game-canvas');
+window.addEventListener('DOMContentLoaded', () => {
+  const gameTrigger = document.querySelector('.js-game-trigger');
+  const gameWrapper = document.querySelector('.js-game-wrapper');
+  const canvas = gameWrapper.querySelector('#game-canvas');
 
-const gameWrapperSize = gameWrapper.getBoundingClientRect();
-canvas.width = gameWrapperSize.width;
-canvas.height = gameWrapperSize.height;
+  const gameWrapperSize = gameWrapper.getBoundingClientRect();
+  canvas.width = gameWrapperSize.width;
+  canvas.height = gameWrapperSize.height;
 
-// On resize update canvas size
+  // On resize update canvas size
 
-const gameTriggerSize = gameTrigger.getBoundingClientRect();
-const game = new Game(canvas, {
-  offsetX: gameTriggerSize.x,
-  offsetY: gameTriggerSize.y
-});
+  const gameTriggerSize = gameTrigger.getBoundingClientRect();
+  const game = new Game(canvas, {
+    offsetX: gameTriggerSize.x,
+    offsetY: gameTriggerSize.y
+  });
 
-gameTrigger.addEventListener('click', () => {
-  game.start();
+  gameTrigger.addEventListener('click', () => {
+    game.start();
 
-  const heroContentElement = document.querySelector('.js-main-hero-section-content');
-  heroContentElement.addEventListener('transitionend', () => {
-    heroContentElement.remove();
+    const heroContentElement = document.querySelector('.js-main-hero-section-content');
+    heroContentElement.addEventListener('transitionend', () => {
+      heroContentElement.remove();
+    }, {once: true});
+
+    heroContentElement.classList.add('is-hidden');
   }, {once: true});
 
-  heroContentElement.classList.add('is-hidden');
+  // game.showTrigger();
+  window.game = game;
 }, {once: true});
-
-game.showTrigger();
