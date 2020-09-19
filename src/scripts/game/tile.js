bba18a126e0064a81049252d2e314573ae11a4be
Stjepan Petrusa <stjepan.petrusa@gmail.com>
2020-09-19 11:39:55 +0200

    Update tile to use sprite and independent coordinate system

--- src/scripts/game/tile.js
+++ src/scripts/game/tile.js
@@ -1,17 +1,14 @@
+import spriteConfig from './utils/sprite';
+import config from './config';
+
 export const TILE_SIZE = 64;
 
 export default class Tile {
-  constructor({context, i, j, terrainGradient, initialDelay, tilesContainer}) {
-    this.context = context;
-    this.i = i;
-    this.j = j;
+  constructor({terrainGradient, sprite}) {
     this.terrainGradient = terrainGradient;
-    this.initialDelay = initialDelay;
-    this.tilesContainer = tilesContainer;
+    this.sprite = sprite;
 
     this.updateTerrainColor();
-
-    this.hasReward = Math.random() < 0.01;
   }
 
   get elevation() {
@@ -23,91 +20,53 @@ export default class Tile {
     return 2;
   }
 
-  get x() {
-    return (this.i - 1) * TILE_SIZE + this.tilesContainer.offsetX;
-  }
-
-  get y() {
-    return (this.j - 1) * TILE_SIZE + this.tilesContainer.offsetY;
-  }
-
-  get topTile() {
-    return this.tilesContainer.tiles[this.i][this.j - 1] || null;
-  }
-
-  get bottomTile() {
-    return this.tilesContainer.tiles[this.i][this.j + 1] || null;
-  }
-
-  get leftTile() {
-    if (this.i > 0) {
-      return this.tilesContainer.tiles[this.i - 1][this.j];
-    }
-
-    return null;
-  }
-
-  get rightTile() {
-    if (this.i < this.tilesContainer.tilesInRow - 1) {
-      return this.tilesContainer.tiles[this.i + 1][this.j];
-    }
-
-    return null;
-  }
-
-  draw(context) {
-    // context.drawImage(myImage, 0, 0, 16, 16, this.x, this.y, 16, 16);
-
-    if (this.bottomTile && this.bottomTile.elevation === 1 && this.elevation === 2 && this.topTile && this.topTile.elevation === 2) {
-      context.fillStyle = 'saddlebrown';
+  render(context) {
+    if (config.useSpriteForTerrain) {
+      context.drawImage(
+        this.sprite,
+        this.spriteConfig.x,
+        this.spriteConfig.y,
+        spriteConfig.tileSize,
+        spriteConfig.tileSize,
+        0,
+        0,
+        TILE_SIZE,
+        TILE_SIZE
+      );
     } else {
       context.fillStyle = this.terrainColor;
-    }
-
-    context.fillRect(this.x, this.y, TILE_SIZE, TILE_SIZE);
-
-    if (this.leftTile && this.leftTile.elevation === 1 && this.elevation === 2) {
-      context.fillStyle = 'saddlebrown';
-      context.fillRect(this.x, this.y, 5, TILE_SIZE);
-    }
-
-    if (this.rightTile && this.rightTile.elevation === 1 && this.elevation === 2) {
-      context.fillStyle = 'saddlebrown';
-      context.fillRect(this.x + TILE_SIZE - 5, this.y, 5, TILE_SIZE);
-    }
-
-    if (this.hasReward) {
-      context.fillStyle = 'yellow';
-      context.fillRect(this.x + TILE_SIZE / 2 - 5, this.y + TILE_SIZE / 2 - 5, 10, 10);
+      context.fillRect(0, 0, TILE_SIZE, TILE_SIZE);
     }
   }
 
   updateTerrainColor() {
     if (this.terrainGradient < 0.05) {
       this.terrainColor = '#425BBD';
+      this.spriteConfig = spriteConfig.tiles.water2;
     } else if (this.terrainGradient < 0.15) {
       this.terrainColor = '#4884D4';
+      this.spriteConfig = spriteConfig.tiles.water2;
     } else if (this.terrainGradient < 0.2) {
       this.terrainColor = '#45A1DE';
+      this.spriteConfig = spriteConfig.tiles.water1;
     } else if (this.terrainGradient < 0.25) {
       this.terrainColor = '#FFE88C';
+      this.spriteConfig = spriteConfig.tiles.sand1;
     } else if (this.terrainGradient < 0.5) {
       this.terrainColor = '#C3D442';
+      this.spriteConfig = spriteConfig.tiles.grass1;
     } else if (this.terrainGradient < 0.6) {
       this.terrainColor = '#82AA28';
+      this.spriteConfig = spriteConfig.tiles.grass1;
     } else if (this.terrainGradient < 0.75) {
       this.terrainColor = '#597F1E';
+      this.spriteConfig = spriteConfig.tiles.grass1;
     } else if (this.terrainGradient < 0.95) {
-      // this.terrainColor = '#B9B5C3';
       this.terrainColor = '#82AA28';
+      this.spriteConfig = spriteConfig.tiles.grass2;
     } else {
-      // this.terrainColor = '#F2F2F0';
       this.terrainColor = '#597F1E';
+      this.spriteConfig = spriteConfig.tiles.grass2;
     }
-  }
-
-  setPosition(i, j) {
-    this.i = i;
-    this.j = j;
   }
 }
